d557ced36fbab761be8a79f18675e0e29e0935a5
rafaelsmiguel <rafael.souza.miguel@gmail.com>
2020-10-06 23:11:30 -0300

    Adicionado classes que serão utilizadas no app

---
+++ SuperHotelApp/SuperHotelApp/Booking/BookingListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BookingListViewController.swift
+//  SuperHotel
+//
+//  Created by Rafael Miguel on 21/09/20.
+//  Copyright © 2020 Rafael Miguel. All rights reserved.
+//
+
+import UIKit
+
+class BookingListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SuperHotelApp/SuperHotelApp/Hotel/SearchHotelViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SearchHotelViewController.swift
+//  SuperHotel
+//
+//  Created by Rafael Miguel on 21/09/20.
+//  Copyright © 2020 Rafael Miguel. All rights reserved.
+//
+
+import UIKit
+
+class SearchHotelViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
